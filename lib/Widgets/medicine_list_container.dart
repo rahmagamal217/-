2f271cb270bae3f8d5ff30f86c9b8f0cd7217fe6
rahmagamal217 <113554331+rahmagamal217@users.noreply.github.com>
2023-03-21 18:23:48 +0200

    adding slidable commit

--- lib/Widgets/medicine_list_container.dart
+++ lib/Widgets/medicine_list_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:medicine_remainder/constants.dart';
 
 class MedicineListContainer extends StatelessWidget {
@@ -27,53 +28,66 @@ class MedicineListContainer extends StatelessWidget {
           elevation: 5,
           child: GestureDetector(
             onTap: (() {}),
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(25),
-                ),
+            child: Slidable(
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    onPressed: ((context) {}), //deleteFunction,
+                    icon: Icons.delete,
+                    backgroundColor: Colors.red.shade300,
+                    borderRadius: BorderRadius.circular(12),
+                  )
+                ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18),
-                child: Row(
-                  children: [
-                    IconButton(
-                      alignment: Alignment.topRight,
-                      onPressed: () {},
-                      icon: const Icon(Icons.edit),
-                      color: customColor3,
-                    ),
-                    SizedBox(
-                      width: size.width * 0.1,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Text(
-                              name,
-                              style: const TextStyle(fontSize: 23),
-                            ),
-                            const SizedBox(
-                              height: 18,
-                            ),
-                            Text(
-                              'الجرعه التاليه : $nextDose ',
-                              style:
-                                  TextStyle(fontSize: 18, color: customColor),
-                            ),
-                          ],
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        alignment: Alignment.topRight,
+                        onPressed: () {},
+                        icon: const Icon(Icons.edit),
+                        color: customColor3,
+                      ),
+                      SizedBox(
+                        width: size.width * 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Text(
+                                name,
+                                style: const TextStyle(fontSize: 23),
+                              ),
+                              const SizedBox(
+                                height: 18,
+                              ),
+                              Text(
+                                'الجرعه التاليه : $nextDose ',
+                                style:
+                                    TextStyle(fontSize: 18, color: customColor),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Image.asset(
-                      image,
-                      width: size.width * 0.35,
-                      height: 40,
-                      alignment: Alignment.centerLeft,
-                    ),
-                  ],
+                      Image.asset(
+                        image,
+                        width: size.width * 0.35,
+                        height: 40,
+                        alignment: Alignment.centerLeft,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
